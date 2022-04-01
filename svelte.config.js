441803fd42b2991a39cab895347c34e24a639f11
KScriven <kerryn.scriven@bbc.co.uk>
2022-04-01 09:36:18 +0100

    Update to config files

--- svelte.config.js
+++ svelte.config.js
@@ -2,12 +2,6 @@ import netlify from '@sveltejs/adapter-netlify';
 
 export default {
 	kit: {
-		adapter: netlify(),
-		target: '#svelte',
-		vite: {
-			ssr: {
-				noExternal: Object.keys(pkg.dependencies || {})
-			}
-		}
+		adapter: netlify()
 	}
 };

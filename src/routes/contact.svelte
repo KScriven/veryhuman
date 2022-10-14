83076fa2ce5686f7cf9d119b245c541b43bc596c
Kerryn Scriven <Kerryn.scriven@bbc.co.uk>
2022-10-14 09:38:23 +0100

    Update to contact page

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,9 +1,6 @@
 <header>
-	<ul>
-		<li>
-			<a href="/">[Home]</a>
-		</li>
-	</ul>
+	<a href="/">[home]</a>
+	<a href="https://www.linkedin.com/in/kerrynscriven/">[linkedIn]</a>
 </header>
 <main>
 	<h1>How to shoot the breeze</h1>
@@ -13,11 +10,7 @@
 			Whilst this means I am not always up to date with the latest thing, I am happy to sacrifice
 			that for more focussed time spent online or elsewhere.
 		</p>
-		<p>
-			You are welcome to email me at kerryn.lloyd[at]gmail.com or find me on <a
-				href="https://www.linkedin.com/in/kerrynscriven/">[LinkedIn]</a
-			>
-		</p>
+		<p>You are welcome to email me at kerryn.lloyd[at]gmail.com or find me on LinkedIn</p>
 		<p>Learning to code is hard work so you can keep me motivated by being kind.</p>
 	</div>
 </main>
@@ -25,7 +18,6 @@
 <style>
 	header {
 		display: flex;
-		/* padding: 0rem 1.5rem; */
 		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu',
 			'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
 		-webkit-font-smoothing: antialiased;
@@ -39,36 +31,25 @@
 		-moz-osx-font-smoothing: grayscale;
 	}
 	h1 {
-		font-size: 4rem;
-		text-align: left;
-		position: relative;
+		margin-top: 0.1rem;
+		margin-bottom: 0.1rem;
 	}
-
-	ul {
-		list-style-type: none;
-		text-align: left;
-	}
-
 	a {
-		text-decoration: none;
 		color: white;
 		font-size: 1.2rem;
-		display: block;
-		padding: 0.5rem 0.5rem;
-	}
-
-	a:hover {
+		display: inline;
+		padding: 0.5rem;
 		background: linear-gradient(90deg, #fc466b 0%, #3f5efb 100%);
 	}
-
-	@media screen and (max-width: 600px) {
-		h1 {
-			display: inline;
-			padding-top: 2rem;
-			font-size: 6vw;
+	@media screen and (max-width: 450px) {
+		a {
+			text-decoration: none;
+			color: white;
+			font-size: 1.2rem;
+			padding: 0.5rem;
 		}
 		p {
-			padding-top: 1rem;
+			padding-top: 0.2rem;
 		}
 	}
 </style>
